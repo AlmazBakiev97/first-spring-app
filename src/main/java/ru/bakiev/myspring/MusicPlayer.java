c78649fb2428324942a2ea001574cc5697accc5c
Almaz Bakiev <almazbakiev97@mail.ru>
2025-04-15 12:54:08 +0300

    add annotatins:scope, value

--- src/main/java/ru/bakiev/myspring/MusicPlayer.java
+++ src/main/java/ru/bakiev/myspring/MusicPlayer.java
@@ -2,21 +2,44 @@ package ru.bakiev.myspring;
 
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.beans.factory.annotation.Qualifier;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.context.annotation.Scope;
 import org.springframework.stereotype.Component;
 
 import java.util.Random;
 
 @Component
+@Scope("prototype")
 public class MusicPlayer {
     private ClassicalMusic classicalMusic;
     private RockMusic rockMusic;
     private HipHopMusic hipHopMusic;
+    @Value("${musicPlayer.name}")
+    private String name;
+    @Value("${musicPlayer.volume}")
+    private int volume;
 
     @Autowired
     public MusicPlayer(ClassicalMusic classicalMusic, RockMusic rockMusic, HipHopMusic hipHopMusic) {
         this.classicalMusic = classicalMusic;
         this.rockMusic = rockMusic;
         this.hipHopMusic = hipHopMusic;
+    }
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public int getVolume() {
+        return volume;
+    }
+
+    public void setVolume(int volume) {
+        this.volume = volume;
     }
 
     public void playMusic(SongsGenre songGenre) {
--- src/main/java/ru/bakiev/myspring/TestSpring.java
+++ src/main/java/ru/bakiev/myspring/TestSpring.java
@@ -7,10 +7,16 @@ public class TestSpring {
         ClassPathXmlApplicationContext context = new ClassPathXmlApplicationContext("applicationContext.xml");
 
         MusicPlayer musicPlayer = context.getBean("musicPlayer", MusicPlayer.class);
+        MusicPlayer musicPlayer2 = context.getBean("musicPlayer", MusicPlayer.class);
 
         musicPlayer.playMusic(SongsGenre.CLASSICAL);
         musicPlayer.playMusic(SongsGenre.ROCK);
         musicPlayer.playMusic(SongsGenre.HIPHOP);
+
+        System.out.println(musicPlayer.getName());
+        System.out.println(musicPlayer.getVolume());
+
+        System.out.println(musicPlayer == musicPlayer2);
 
         context.close();
     }
